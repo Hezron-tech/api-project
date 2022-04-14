61799b8d008a1f1d2a692084fd9dc4b1e2a78bc4
Hezron-tech <kiprophez@gmail.com>
2022-04-14 22:03:31 +0300

    creating userviewset

--- api/serializers.py
+++ api/serializers.py
@@ -7,3 +7,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url','username','email','groups']
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url','name']
